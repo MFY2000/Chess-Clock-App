f4c51ce145077e5d30a72be9945ad3b660420bec
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-05 23:24:30 +0500

    Update Timer.dart

--- lib/Custom/Timer.dart
+++ lib/Custom/Timer.dart
@@ -1,10 +1,47 @@
+// ignore_for_file: file_names
 
 
 void main() {
-  Map<String, int> Timer = {"Hours": 00, "Minutes": 00, "Seconds":00};
+  var obj = Timer(Time: {"Hours": 00, "Minutes": 10, "Seconds":00});
 
-  print(Timer);
+  print(obj);
 
+
+}
+
+class Timer {
+  Map<String, int> Time;
+  late int status; // 1 => Running, 0 => Stop, 3 => Timeover 
+
+
+  Timer({required this.Time});
+
+  
+  void Start(){
+    status = 1;
+    Running();
+  }
+  
+  Future<void> Running() async {
+    if(status == 1)
+      await Future.delayed(const Duration(seconds: 1), (){ afterASecond(); });
+  }
+
+  afterASecond(){
+    int? Seconds = Time["Seconds"];
+    if(Seconds == 0){
+      if(Time["Minutes"] == 0){
+        
+      }
+      Time["Seconds"] = 59;
+      Time["Minutes"] = (Time["Minutes"]!-1);
+    }
+    else{
+      Time["Seconds"] = (Seconds! - 1) ;
+    }
+
+    Running();
+  }
 
 }
 
