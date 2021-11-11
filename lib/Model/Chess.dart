b154b5136dc71b5111b3024bf38ab264504d63cc
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-11 21:26:37 +0500

    MFY auto commit at 11/11/2021 21:25:14

--- lib/Model/Chess.dart
+++ lib/Model/Chess.dart
@@ -4,13 +4,13 @@
 
 import 'dart:ui';
 
+import 'package:chesss_watch/Custom/Timer.dart';
 import 'package:flutter/material.dart';
 
 class ChessPlayer {
   late final String name;
   final String image;
   final Color color;
-
 
   ChessPlayer({required this.color, required this.image});
 }
