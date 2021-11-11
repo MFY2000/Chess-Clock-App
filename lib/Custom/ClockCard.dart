579eff2ced0af64abc3be4bf84fe028ad4070c48
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-11 21:26:32 +0500

    MFY auto commit at 11/11/2021 21:25:14

---
+++ lib/Custom/ClockCard.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+
+class ClockCard extends StatefulWidget {
+  const ClockCard({ Key? key }) : super(key: key);
+
+  @override
+  _ClockCardState createState() => _ClockCardState();
+}
+
+class _ClockCardState extends State<ClockCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
