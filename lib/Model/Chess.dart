ee2d4acf8cfc38cf7c28d2d141a8e4615c91e3ee
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-11 22:56:36 +0500

    MFY auto commit at 11/11/2021 22:56:29

--- lib/Model/Chess.dart
+++ lib/Model/Chess.dart
@@ -10,13 +10,14 @@ import 'package:flutter/material.dart';
 class ChessPlayer {
   late final String name;
   final String image;
-  final Color color;
+  final Color color1;
+  final Color color2;
 
-  ChessPlayer({required this.color, required this.image});
+  ChessPlayer({required this.color1,required this.color2, required this.image});
 }
 
 List<ChessPlayer> ChessBoard = [
-  ChessPlayer(color: Colors.black, image: "images/1.png"),
-  ChessPlayer(color: Colors.white, image: "images/2.png"),
+  ChessPlayer(color1: Colors.black, color2: Colors.white, image: "images/2.png"),
+  ChessPlayer(color1: Colors.white, color2: Colors.black, image: "images/1.png"),
 
 ];
