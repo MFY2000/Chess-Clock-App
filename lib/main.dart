58021e0d74c92862524305139639b787ace77c26
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-14 09:25:38 +0500

    MFY auto commit at 14/11/2021 09:25:01

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,9 @@ import 'package:chesss_watch/Model/Chess.dart';
 import 'package:chesss_watch/Screen/StartInfroScreen.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -23,81 +19,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chess Watch',
-      
-      home: StartScreen(),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
-
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key}) : super(key: key);
-
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
-
+class _MyHomePageState extends State<MyHomePage> {
   late int start;
-  late List<ChessPlayer> lst;
-  TimerStyle _timerStyle = TimerStyle.ring;
-  TimerProgressTextCountDirection _progressTextCountDirection = TimerProgressTextCountDirection.count_down;
+  late bool startButton;
 
+  late List<ChessPlayer> lst;
+  final TimerStyle _timerStyle = TimerStyle.ring;
+  TimerProgressTextCountDirection _progressTextCountDirection =
+      TimerProgressTextCountDirection.count_down;
+  late double height, width;
 
   @override
   void initState() {
     // initialize timercontroller
     lst = ChessBoard;
+    startButton = true;
     start = 2;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      
       body: Column(
         children: <Widget>[
           ClockCard(
             chessSide: ChessBoard[1],
-            start: start == 1 ,
-            timerStyle: _timerStyle,
-            onTapClock: onTapStart,
-            progressTextCountDirection: _progressTextCountDirection,
-          ),
-
-          ClockCard(
-            chessSide: ChessBoard[0],
             start: start == 0,
             timerStyle: _timerStyle,
             onTapClock: onTapStart,
             progressTextCountDirection: _progressTextCountDirection,
           ),
-          
-          
-          
 
-          ],
+          TextButton(
+            onPressed: toChangeState,
+            // autofocus: false,
+            child: Container(
+              color: startButton ? Colors.green[600] : Colors.red,
+
+              height: (height * 0.045),
+              width: (width * 0.25),
+              alignment: Alignment.center,
+              child: Text(
+                startButton ? 'Start' : "Stop",
+                style: TextStyle(
+                  fontFamily: 'Times New Roman',
+                  color: Color(0xffffffff),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          ClockCard(
+            chessSide: ChessBoard[0],
+            start: start == 1,
+            timerStyle: _timerStyle,
+            onTapClock: onTapStart,
+            progressTextCountDirection: _progressTextCountDirection,
+          ),
+        ],
       ),
     );
   }
 
-  void onTapStart(){
+  void onTapStart() {
     setState(() {
-      start = start == 0 ? 1: 0;
+      start = start == 0 ? 1 : 0;
     });
   }
 
+  void toChangeState() {
+    setState(() {
+      startButton = !startButton;
+    });
+
+    if(!startButton){
+      setState(() {
+        start = 2;
+      });
+    }else{
+      onTapStart();
+    }
+
+  }
 
   void _setCountDirection(TimerProgressTextCountDirection countDirection) {
     setState(() {
       _progressTextCountDirection = countDirection;
     });
   }
-
 }
-
