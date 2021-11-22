8a406ab5903847904c62b59cbd787c2ad8cc5d02
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-22 23:07:24 +0500

    MFY auto commit at 22/11/2021 23:07:12

--- lib/Model/Chess.dart
+++ lib/Model/Chess.dart
@@ -4,22 +4,22 @@
 
 import 'dart:ui';
 
-import 'package:chesss_watch/Custom/Timer.dart';
 import 'package:flutter/material.dart';
 
 class ChessPlayer {
+  final int id;
   late final String name;
   final String image;
   final Color color1;
   final Color color2;
 
-  ChessPlayer({required this.color1,required this.color2, required this.image});
+  ChessPlayer({required this.id ,required this.color1,required this.color2, required this.image});
 }
 
 List<ChessPlayer> ChessBoard = [
-  ChessPlayer(color1: Colors.black, color2: Colors.white, image: "assets/images/2.png"),
-  ChessPlayer(color1: Colors.white, color2: Colors.black, image: "assets/images/1.png")
+  ChessPlayer(id: 0, color1: Colors.black, color2: Colors.white, image: "assets/images/2.png"),
+  ChessPlayer(id: 1 ,color1: Colors.white, color2: Colors.black, image: "assets/images/1.png")
 ];
 
-List<int> historyLog = [2];
+List<int> historyLog = [1];
 
